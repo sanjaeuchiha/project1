739794a97f00f8a08bc3f561d66e059359b29664
sanjaeuchiha <sanjae_allen@hotmail.com>
2015-02-28 05:54:03 +0000

    added db info

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,6 @@ from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://lcjsohpzwnnvqo:_hdakVe7QefNX9jfJd9R-AvceA@ec2-23-21-231-14.compute-1.amazonaws.com:5432/d318k172garutn'                                         
-db= SQLAlchemy(app)
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://jrjrqlznydduow:Ydcaj6hw2OJIyMU7Os0Kcy5lDo@ec2-107-21-100-118.compute-1.amazonaws.com:5432/d5i7p4d102h8ad'                                      db= SQLAlchemy(app)
  
 from app import views, models
